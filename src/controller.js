be95a346c6baacf109216d5cd5538ee2f6c4ca8a
DaianaZabala <daianazabala1303@gmail.com>
2023-10-25 12:27:02 -0300

    Configuración servicio base de dato(sin evaluacion de errores)

--- src/controller.js
+++ src/controller.js
@@ -23,8 +23,27 @@ class LibroController{
             console.log(e);
         }
        
-       
+           }
+
+    async add(req, res){
+        const libro = req.body;
+        const [result] = await pool.query(`INSERT INTO Libros (nombre, autor, categoria, aNopublicacion, ISBN) VALUES    (?, ?, ?, ?, ?)`, [libro.nombre, libro.autor, libro.categoria, libro.aNopublicacion, libro.ISBN]);
+        res.json({"Nuevo Libro insertado": result.insertId});
     }
+   
+    async update(req, res){
+        const libro = req.body;
+        const [result] = await pool.query(`UPDATE Libros SET nombre=(?), autor=(?) , categoria=(?), aNopublicacion=(?), ISBN=(?) WHERE id=(?)` , [libro.nombre, libro.autor, libro.categoria, libro.aNopublicacion, libro.ISBN, libro.id] );
+        res.json({"Libros actualizados": result.changedRows});
+    }
+
+    async delete(req, res){
+        const libro = req.body;
+        const [result] = await pool.query(`DELETE FROM Libros WHERE ISBN=(?)`, [libro.ISBN]);
+        res.json({"Libro eliminado": result.affectedRows});
+    }
+
+    async 
 }
 
 export const libro = new LibroController();
--- src/routes.js
+++ src/routes.js
@@ -5,3 +5,6 @@ export const router = Router()
 
 router.get('/libros',  libro.getAll);
 router.get('/libro', libro.getOne);
+router.post('/libro', libro.add);
+router.delete('/libro', libro.delete);
+router.put('/libro', libro.update);
